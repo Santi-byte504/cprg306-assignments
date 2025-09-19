5c4a48744f817fdd9c93119b8f80b8f7880a053b
Santi-byte504 <Santiago.Zonco@edu.sait.ca>
2025-09-19 14:05:43 -0600

    Create student-info.js

---
+++ app/week-2/student-info.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+export default function StudentInfo() {
+  return (
+    <main>
+      <h1>Name: Santi Zonco</h1>
+      <h1>
+        GitHub:{" Santi-byte504/cprg306-assignments"}
+        <Link
+          href="https://github.com/Santi-byte504/cprg306-assignments"
+        >
+        </Link>
+      </h1>
+    </main>
+  );
+}
