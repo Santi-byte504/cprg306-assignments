32415adcd5aa7dcb0edb7a05455ddb9c5719b93c
Santi-byte504 <Santiago.Zonco@edu.sait.ca>
2025-09-26 14:03:23 -0600

    Update page.js

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,1 +1,9 @@
-
+import ItemList from "./item-list";
+export default function Page() {
+  return (
+    <main className="p-4 max-w-xl mx-auto">
+      <h1 className="text-2xl font-bold mb-3">Shopping List</h1>
+      <ItemList />
+    </main>
+  );
+}
