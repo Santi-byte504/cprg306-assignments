8e305c706013aed2671aedd48836b5da4c799e8e
Santi-byte504 <Santiago.Zonco@edu.sait.ca>
2025-09-26 14:02:29 -0600

    Update item.js

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,1 +1,9 @@
-
+export default function Item({Name, Quantity, Category}){
+    return(
+        <li className="rounded bg-gray-900 p-2">
+            <p>{Name}</p>
+            <p>Quantity: {Quantity}</p>
+            <p className="capitalize">Category: {Category}</p>
+        </li>
+    
+    )}
