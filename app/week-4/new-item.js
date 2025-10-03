e0527b093a8db5ce5f4707393e8e1a321c1c01c8
Santi-byte504 <Santiago.Zonco@edu.sait.ca>
2025-10-02 20:49:55 -0600

    Update new-item.js

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -1,1 +1,24 @@
-
+"use client";
+import {useState} from "react";
+export default function NewItem(){
+    const [Quantity, SetQuantity] = useState(1);
+    const increment = () => {
+        SetQuantity((quantity) => Math.min(20, quantity + 1));
+    }
+    const decrement = () => {
+        SetQuantity((quantity) => Math.max(1, quantity - 1));
+    }
+    return(
+            <section className="rounded p-4 bg-gray-900">
+      <p className="mb-3">
+        <span className="font-bold text-sm">Quantity:{" "}</span>
+        <span className="font-bold text-xl">{Quantity}</span>
+      </p>
+        <div className="flex item-center my-4 gap-3">
+            <button onClick={decrement} disabled={Quantity === 1} className="rounded px-4 py-2 bg-gray-700 hover:bg-gray-300 hover:text-black disabled:bg-red-300 disabled:text-red-700">-</button>
+            <button onClick={increment} disabled={Quantity === 20} className="rounded px-4 py-2 bg-gray-700 hover:bg-gray-300 hover:text-black disabled:bg-red-300 disabled:text-red-700">+</button>
+        </div>
+         <p className="mt-2 text-xs">Allowed range: 1–20</p>
+        </section>
+    )
+}
