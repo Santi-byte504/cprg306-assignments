9fa7267f60d06295a86028b7ac0d0d766ce3db73
Santi-byte504 <Santiago.Zonco@edu.sait.ca>
2025-10-02 20:50:38 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -8,6 +8,7 @@ export default function Main(){
       <p>
         <Link href={"/week-2"}>| Week 2</Link>
         <Link href={"/week-3"}> | Week 3</Link>
+        <Link href={"/week-4"}> | Week 4</Link>
       </p>
     </main>
   )
