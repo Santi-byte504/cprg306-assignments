ed76f4f51964937cb1e619c88835659c94b71a21
Santi-byte504 <Santiago.Zonco@edu.sait.ca>
2025-10-14 19:50:31 -0600

    Update page.js

--- app/week-5/page.js
+++ app/week-5/page.js
@@ -1,1 +1,9 @@
-
+import NewItem from "./new-item";
+export default function NewItemPage() {
+    return(
+        <main className="p-6 max-w-md mx-auto">
+            <h1 className="font-bold text-2xl text-center mb-4">Add New Item</h1>
+            <NewItem />
+        </main>
+    )
+}
