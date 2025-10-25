7da76ec17cec617a6f2d0db4ac5a55ce474b474e
Santi-byte504 <Santiago.Zonco@edu.sait.ca>
2025-10-24 20:40:56 -0600

    Implement shopping list page with item addition

--- app/week-7/page.js
+++ app/week-7/page.js
@@ -1,1 +1,20 @@
+"use client";
+import ItemList from "./item-list";
+import NewItem from "./new-item";
+import itemsData from "./items.json";
+import { useState } from "react";
 
+export default function Page() {
+const [items, otherItems] = useState(itemsData);
+const handleAddItem = (item) => {
+  otherItems((Itemscurrent) => [...Itemscurrent, item]);
+}
+
+  return (
+    <main className="p-4 max-w-xl mx-auto space-y-4">
+      <h1 className="text-2xl font-bold mb-3">Shopping List</h1>
+      <NewItem onAddItem={handleAddItem} />
+      <ItemList items = {items} />
+    </main>
+  );
+}
