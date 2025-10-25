ed0de4f913828f9c7c7af074cdc1c62cfe14f286
Santi-byte504 <Santiago.Zonco@edu.sait.ca>
2025-10-24 20:40:18 -0600

    Add Item component to display item details

--- app/week-7/item.js
+++ app/week-7/item.js
@@ -1,1 +1,9 @@
-
+export default function Item({name, quantity, category}){
+    return(
+        <li className="rounded bg-gray-900 p-2">
+            <p>{name}</p>
+            <p>Quantity: {quantity}</p>
+            <p className="capitalize">Category: {category}</p>
+        </li>
+    
+    )}
